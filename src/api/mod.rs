50cd0cd06457ecd3bd0992b9f7cc372059d73156
thorrester <steven.forrester@shipt.com>
2023-11-26 20:06:29 +0000

    copyright

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,3 +1,6 @@
+/// Copyright (c) Shipt, Inc.
+/// This source code is licensed under the MIT license found in the
+/// LICENSE file in the root directory of this source tree.
 pub mod command_structs;
 pub mod download_file;
 pub mod list_cards;
