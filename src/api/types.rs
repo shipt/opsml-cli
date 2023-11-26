fce13b1292fa58626646098c59a5d3f6336d4ce3
thorrester <steven.forrester@shipt.com>
2023-11-26 20:06:29 +0000

    copyright

--- src/api/types.rs
+++ src/api/types.rs
@@ -1,3 +1,6 @@
+/// Copyright (c) Shipt, Inc.
+/// This source code is licensed under the MIT license found in the
+/// LICENSE file in the root directory of this source tree.
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
